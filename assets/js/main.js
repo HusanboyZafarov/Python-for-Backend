5e7b7712e16af9c2fe2aad500782114009792b66
HusanboyZafarov <115022906+HusanboyZafarov@users.noreply.github.com>
2023-02-07 18:50:59 +0500

    Fixing bugs

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,18 +6,3 @@ window.addEventListener("scroll", () => {
     up.style.height = header_enter_course.getBoundingClientRect().height
     header_enter_course.classList.toggle("scrolled", window.scrollY > 500)
 })
-
-
-
-let contact_form_input = document.querySelectorAll(".contact_form input")
-let contact_form_label = document.querySelectorAll(".contact_form label")
-contact_form_input.forEach((el) => {
-    el.addEventListener("click", () => {
-        label = el.previousElementSibling
-        contact_form_label.forEach((item) => {
-            item.classList.remove("hovered")
-        });
-        label.classList.add("hovered")
-    })
-}
-)
