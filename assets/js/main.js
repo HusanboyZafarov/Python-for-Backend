23873c3550947145656aeaf7a02696fc8494f0a1
HusanboyZafarov <115022906+HusanboyZafarov@users.noreply.github.com>
2023-02-07 16:06:57 +0500

    Updating

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+let up = document.querySelector(".up"),
+    header_enter_course = document.querySelector(".header_enter_course")
+
+window.addEventListener("scroll", () => {
+    up.classList.toggle("scrolled", window.scrollY > 500)
+    up.style.height = header_enter_course.getBoundingClientRect().height
+    header_enter_course.classList.toggle("scrolled", window.scrollY > 500)
+})
