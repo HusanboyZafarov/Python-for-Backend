127ed9f48a0f8a59872831792f7c5af2c83ea2c3
HusanboyZafarov <115022906+HusanboyZafarov@users.noreply.github.com>
2023-02-07 17:55:04 +0500

    Adding countdown to html

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,22 @@ window.addEventListener("scroll", () => {
     up.style.height = header_enter_course.getBoundingClientRect().height
     header_enter_course.classList.toggle("scrolled", window.scrollY > 500)
 })
+
+var countDownDate = new Date("Feb 20, 2023 17:35:25").getTime();
+
+var x = setInterval(function () {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.querySelector(".day").innerHTML = days
+    document.querySelector(".hour").innerHTML = hours
+    document.querySelector(".min").innerHTML = minutes
+    document.querySelector(".second").innerHTML = seconds
+    if (countDownDate < now) {
+        clearInterval(x);
+        document.querySelector(".hero-coundown").innerHTML = "Vaqt tugadi!";
+    }
+}, 1000);
